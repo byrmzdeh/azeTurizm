af42fec03f28f37b8ffefcd5537853159583bbba
byrmzdeh <bayramzade063@gmail.com>
2025-08-13 10:44:19 +0400

    detail-slider

--- js/components/slider.js
+++ js/components/slider.js
@@ -1,3 +1,25 @@
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 0,
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 6,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+
+
 // slider
 let intervalNum = 3000;
 let sliderCount = 1;
@@ -107,24 +129,7 @@ var swiper = new Swiper(".internalSwiper", {
 
 
 
-var swiper = new Swiper(".mySwiper", {
-  loop: true,
-  spaceBetween: 0,
-  slidesPerView: 6,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".mySwiper2", {
-  loop: true,
-  spaceBetween: 6,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
-});
+
 
 
 
